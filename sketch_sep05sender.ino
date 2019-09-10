e554ca629ba06b9536426f81a254acdd43d1c663
mehedehasanrakib <55144241+mehedehasanrakib@users.noreply.github.com>
2019-09-11 00:39:13 +0600

    sender

---
+++ sketch_sep05sender.ino
@@ -0,0 +1,27 @@
+int sensor = A0;
+int sensorValue = 0;
+
+
+
+
+  
+
+
+void setup()
+{
+  pinMode(sensor, INPUT);
+  Serial.begin(9600);
+}
+
+
+void loop()
+{
+sensorValue = analogRead(sensor);
+
+Serial.println(sensorValue);
+Serial.print(";");
+
+delay(200);
+
+
+}
